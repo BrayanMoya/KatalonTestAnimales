04f2a59dc56a5541297e52981e595b0838ccf1f1
bmoya <bmoya@DESKTOP-QABIK30>
2020-06-11 21:14:06 -0500

    katalon

---
+++ Test Suites/katalon.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>katalon</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8bd1973f-1f66-467c-be40-315b674a0dfc</testSuiteGuid>
+   <testCaseLink>
+      <guid>e4f05aae-49b3-4658-b4e4-a8499e50555c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/katalon</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
